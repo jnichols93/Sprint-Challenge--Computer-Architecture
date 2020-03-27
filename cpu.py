31e11fc79694e95627fc475a9f792029a70be3ae
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-03-27 12:01:38 -0500

    MVP 145

--- cpu.py
+++ cpu.py
@@ -2,50 +2,60 @@
 
 import sys
 
-#create instructions for LDI, PRN, HLT, and MUL programs
+HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
-HLT = 0b00000001
-MUL = 0b10100010
 ADD = 0b10100000
+MUL = 0b10100010
 PUSH = 0b01000101
 POP = 0b01000110
 CALL = 0b01010000
 RET = 0b00010001
 CMP = 0b10100111
+JMP = 0b01010100
 JEQ = 0b01010101
 JNE = 0b01010110
-JMP = 0b01010100
+
 SP = 7
 
+#set flags
+less_flag = 0b00000100
+greater_flag = 0b00000010
+equal_flag = 0b00000001
+end_flag = 0b01001001
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        # setup ram, register, and pc
         self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
-        self.fl = 0
-        self.branchtable = {
-        HLT: self.handle_hlt,
-        LDI: self.handle_ldi,
-        PRN: self.handle_prn,
-        ADD: self.handle_add,
-        MUL: self.handle_mul,
-        PUSH: self.handle_push,
-        POP: self.handle_pop,
-        CALL: self.handle_call,
-        RET: self.handle_ret
-        }
-        
         self.halted = False
-        #register 7 is reserved as the stack pointer, which is 0xf4 per specs
-        self.register[SP] = 0xf4
+        self.fl = 0
+        self.branch_table = {
+            HLT: self.handle_hlt,
+            LDI: self.handle_ldi,
+            PRN: self.handle_prn,
+            ADD: self.handle_add,
+            MUL: self.handle_mul,
+            PUSH: self.handle_push,
+            POP: self.handle_pop,
+            CALL: self.handle_call,
+            RET: self.handle_ret,
+            CMP: self.handle_cmp,
+            JMP: self.handle_jmp,
+            JEQ: self.handle_jeq,
+            JNE: self.handle_jne
+        }
+
+    
+
+    
 
     def load(self):
+        """Load a program into memory."""
         """Load a program into memory."""
 
         if len(sys.argv) != 2:
@@ -54,18 +64,6 @@ class CPU:
 
         program = sys.argv[1]
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
 
         #open file
         with open(program) as file:
@@ -82,16 +80,20 @@ class CPU:
                 value = int(val, 2)
                 self.ram[address] = value
                 address +=1
-        
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.register[reg_a] += self.register[reg_b]
         elif op == "MUL":
             self.register[reg_a] *= self.register[reg_b]
+        elif op == "CMP":
+            if self.register[reg_a] < self.register[reg_b]:
+                self.fl = less_flag
+            elif self.register[reg_a] > self.register[reg_b]:
+                self.fl = greater_flag
+            else:
+                self.fl = equal_flag
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -103,97 +105,94 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
 
-    #ram_read should accept an address to read and return the value
-    def ram_read(self, mar):
-        return self.ram[mar]
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
-    #ram_write should accept a value to write, and the address to write it to
-    def ram_write(self, mdr, mar):
-        self.ram[mar] = mdr
-
-    
-    def handle_ldi(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        
+    def handle_ldi(self, operand_a, operand_b):
         self.register[operand_a] = operand_b
+        self.pc += 3
 
-    def handle_prn(self):
-        operand_a = self.ram_read(self.pc + 1)
+    def handle_prn(self, operand_a, operand_b):
         print(self.register[operand_a])
+        self.pc += 2
 
-    def handle_hlt(self):
+    def handle_hlt(self, operand_a, operand_b):
         self.halted = True
-    #method to handle adding
-    def handle_add(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        self.alu("ADD", operand_a, operand_b)
 
-    def handle_mul(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        self.alu("MUL", operand_a, operand_b)
+    def handle_add(self, operand_a, operand_b):
+        self.alu('ADD', operand_a, operand_b)
 
-    #method to handle push on the stack
-    def handle_push(self):
+    def handle_mul(self, operand_a, operand_b):
+        self.alu('MUL', operand_a, operand_b)
+
+    def handle_push(self, operand_a, operand_b):
         #decrement the SP register
         self.register[SP] -= 1
-        #set operand_a
-        operand_a = self.ram_read(self.pc + 1)
         # copy the value in the given register to the address pointed to by SP
         operand_b = self.register[operand_a]
         self.ram[self.register[SP]] = operand_b
 
-
-    #method to handle popping from the stack to the register
-    def handle_pop(self):
-        operand_a = self.ram_read(self.pc + 1)
+    def handle_pop(self, operand_a, operand_b):
         # copy the value from the address pointed to by SP to the given register
         operand_b = self.ram[self.register[SP]]
         self.register[operand_a] = operand_b
         #increment the SP
-        self.register[SP] += 1
+        self.pc += 2
 
-    #method to handle subroutine calls
-    def handle_call(self):
+    def handle_call(self, operand_a, operand_b):
         #push address after call to top of stack
         self.register[SP] -= 1
         self.ram[self.register[SP]] = self.pc + 2
         # set the pc to the given register
-        operand_a = self.ram_read(self.pc + 1)
         self.pc = self.register[operand_a]
 
-    #method to handle the return after a call
     def handle_ret(self):
         #return from subroutine
         self.pc = self.ram[self.register[SP]]
         #pop the value from the stack and store in pc                                                                              
         self.register[SP] += 1
 
-    def run(self):
-        while self.halted != True:
-            IR = self.ram[self.pc]
-            val = IR
-            op_count = val >> 6
-            IR_length = op_count + 1
-            self.branchtable[IR]()
+    def handle_cmp(self, operand_a, operand_b):
+        self.alu('CMP', operand_a, operand_b)
+        self.pc += 3
 
-            if IR == 0 or None:
-                print(f"Unknown instructions and index {self.pc}")
-                sys.exit(1)
-            if IR != 80 and IR != 17:
-                self.pc += IR_length
+    def handle_jmp(self, operand_a, operand_b):
+        self.pc = self.register[operand_a]
+
+    def handle_jeq(self, operand_a, operand_b):
+        if self.fl & equal_flag:
+            self.pc = self.register[operand_a]
+        else:
+            self.pc += 2
+
+    def handle_jne(self, operand_a, operand_b):
+        if not self.fl & equal_flag:
+            self.pc = self.register[operand_a]
+        else:
+            self.pc += 2
+
+    def run(self):
+        """Run the CPU."""
+        while self.halted != True:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if IR in self.branch_table:
+                self.branch_table[IR](operand_a, operand_b)
+            else:
+                raise Exception(f'Invalid {IR}, not in branch table \t {list(self.branch_table.keys())}')
